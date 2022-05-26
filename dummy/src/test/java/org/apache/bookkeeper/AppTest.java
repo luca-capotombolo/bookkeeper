592b27df36795671fd6d0526c38da03f4d30d34b
luca-capotombolo <capoluca98@gmail.com>
2022-05-26 17:05:08 +0200

    JACOCO FUNZINANTE IN LOCALE AVVIO PROVA SONAR CLOUD WORKFLOW 1

---
+++ dummy/src/test/java/org/apache/bookkeeper/AppTest.java
@@ -0,0 +1,37 @@
+package org.apache.bookkeeper;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void sum()
+    {
+        App app = new App();
+        int c = app.sum(12, 4);
+        assertEquals(16, c);
+
+    }
+
+    @Test
+    public void sub(){
+        App app = new App();
+        int c = app.sub(12,4);
+        assertEquals(c, 8);
+    }
+
+    @Test
+    public void mul(){
+        App app = new App();
+        int c = app.mul(2,2);
+        assertEquals(c, 4);
+    }
+
+}
